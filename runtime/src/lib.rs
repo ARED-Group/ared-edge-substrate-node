7a2d28d85dff71def3f9046fa4c3f6aca9663704
Peter Nyongesa <ppeterwafula@gmail.com>
2026-01-03 12:47:26 +0300

    revert transaction_version to 1 for V4 format client compatibility

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -118,10 +118,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: sp_runtime::create_runtime_str!("ared-edge"),
     impl_name: sp_runtime::create_runtime_str!("ared-edge-node"),
     authoring_version: 1,
-    spec_version: 103,
+    spec_version: 104,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 2,
+    transaction_version: 1,
     state_version: 1,
 };
 
